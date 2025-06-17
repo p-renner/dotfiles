93a60299f720a8e98101d75a8a84da9db2f043ae
Philipp Renner <phillianus@gmail.com>
2025-06-17 13:25:02 +0200

    feat(oil.nvim): add dependency for folder icons

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -2,7 +2,10 @@ return {
     {
         'stevearc/oil.nvim',
         lazy = false,
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        dependencies = {
+            { 'nvim-tree/nvim-web-devicons', opts = {} },
+            { 'echasnovski/mini.icons', opts = {} },
+        },
         keys = {
             { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
         },
