135de684baccc081598061d776b56b2b8cc3ffe9
Philipp Renner <phillianus@gmail.com>
2026-03-23 15:04:27 +0100

    fix(wezterm): use config_builder and enable Wayland
    Switch to wezterm.config_builder() for better error reporting, and
    remove enable_wayland = false which was forcing XWayland on a Wayland
    system.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,18 +1,17 @@
 local wezterm = require("wezterm")
-local config = {
-	default_prog = { "/bin/zsh" },
-	font = wezterm.font("JetBrainsMono Nerd Font"),
-	font_size = 15.0,
-	color_scheme = "nordfox",
-	enable_wayland = false,
-	hide_tab_bar_if_only_one_tab = true,
-	enable_scroll_bar = true,
-	window_padding = {
-		left = 8,
-		right = 8,
-		top = 8,
-		bottom = 8,
-	},
+local config = wezterm.config_builder()
+
+config.default_prog = { "/bin/zsh" }
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 15.0
+config.color_scheme = "nordfox"
+config.hide_tab_bar_if_only_one_tab = true
+config.enable_scroll_bar = true
+config.window_padding = {
+	left = 8,
+	right = 8,
+	top = 8,
+	bottom = 8,
 }
 
 config.keys = {
